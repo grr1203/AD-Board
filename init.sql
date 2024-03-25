a3502da3db99bff2b7d1918a37b18988b65443c4
grr1203 <gyrms9412@naver.com>
2024-03-25 17:59:49 +0900

    DB DDL 업데이트

--- init.sql
+++ init.sql
@@ -1,8 +1,3 @@
-CREATE TABLE IF NOT EXISTS tb_file (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    ext TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS tb_file (name TEXT NOT NULL);
 
-INSERT INTO tb_file (name, ext) VALUES ('sample.mp4', 'mp4');
+INSERT INTO tb_file (name) VALUES ('sample.mp4');
